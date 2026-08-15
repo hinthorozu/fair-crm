0b6c0b2402b5abacea3093b32564e84381992fb5
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-15 17:12:46 +0300

    Require execute permission migration before dev seed

--- scripts/seed_core_dev_identity.py
+++ scripts/seed_core_dev_identity.py
@@ -46,7 +46,7 @@ CORE_DB_URL = os.environ.get(
     "KYROX_CORE_DATABASE_URL",
     "postgresql://postgres:postgres@localhost:5432/kyrox_core",
 )
-MIN_CORE_MIGRATION_REVISION = "20260814_0047"
+MIN_CORE_MIGRATION_REVISION = "20260815_0049"
 DEV_SEED_ENV_FILE_HINT = "/etc/fair-crm/dev-seed.env"
 
 
