7ee793ccba82c7616322086cebd717671b3f3f39
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-15 18:10:24 +0300

    Require permission rename migration before dev seed

--- scripts/seed_core_dev_identity.py
+++ scripts/seed_core_dev_identity.py
@@ -46,7 +46,7 @@ CORE_DB_URL = os.environ.get(
     "KYROX_CORE_DATABASE_URL",
     "postgresql://postgres:postgres@localhost:5432/kyrox_core",
 )
-MIN_CORE_MIGRATION_REVISION = "20260815_0050"
+MIN_CORE_MIGRATION_REVISION = "20260815_0051"
 DEV_SEED_ENV_FILE_HINT = "/etc/fair-crm/dev-seed.env"
 
 
