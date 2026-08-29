b911946e2b96c4f96bee558dc09282592b2c43ca
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-30 02:07:41 +0300

    fix(ui): add fair action permission constants

---
+++ frontend/src/permissions/fairPermissions.ts
@@ -0,0 +1,4 @@
+export const FAIR_READ = "fair_crm.fairs.read";
+export const FAIR_CREATE = "fair_crm.fairs.create";
+export const FAIR_UPDATE = "fair_crm.fairs.update";
+export const FAIR_DELETE = "fair_crm.fairs.delete";
