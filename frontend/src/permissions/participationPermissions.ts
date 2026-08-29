fd3139f88eeb75c4c04c18c929f5c9d400f18c58
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-30 02:10:54 +0300

    fix(ui): add participation action permission constants

---
+++ frontend/src/permissions/participationPermissions.ts
@@ -0,0 +1,4 @@
+export const PARTICIPATION_READ = "fair_crm.participations.read";
+export const PARTICIPATION_CREATE = "fair_crm.participations.create";
+export const PARTICIPATION_UPDATE = "fair_crm.participations.update";
+export const PARTICIPATION_DELETE = "fair_crm.participations.delete";
